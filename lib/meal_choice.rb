f82bcf02111ab0b7bb8498124cb6558ea699e03f
Shervon Ceromain <shervonc@gmail.com>
2020-01-31 01:51:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,6 @@ puts "What a nutritious meal!"
 
 puts "A plate of #{protein} with #{veg1} and #{veg2}."
 
-
+return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 # For output purposes, use "puts" instead of "print" or "p"
